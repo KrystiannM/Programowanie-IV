0ebcd043a2a7e6ca2b15b7f3046668091693a5f6
KrystiannM <161638008+KrystiannM@users.noreply.github.com>
2024-06-18 23:59:29 +0200

    Add files via upload

---
+++ Services/ToDoListService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.DataModel;
+
+namespace ToDoList.Services
+{
+    public class ToDoListService
+    {
+        public IEnumerable<ToDoItem> GetItems() => new[]
+        {
+            new ToDoItem { Description = "Zrób prezentacje na studia" },
+            new ToDoItem { Description = "Kup mleko, mąke" },
+            new ToDoItem { Description = "Naucz się na egzamin" },
+        };
+    }
+}
